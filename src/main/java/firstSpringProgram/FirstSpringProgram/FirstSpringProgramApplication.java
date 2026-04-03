af8ade9f5880bbfe8d50e9c3d8c4132420aa056d
Deepak Suthar <suthardeepak658@gmail.com>
2026-04-03 08:22:51 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/firstSpringProgram/FirstSpringProgram/FirstSpringProgramApplication.java
@@ -0,0 +1,13 @@
+package firstSpringProgram.FirstSpringProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/firstSpringProgram/FirstSpringProgram/FirstSpringProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package firstSpringProgram.FirstSpringProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
