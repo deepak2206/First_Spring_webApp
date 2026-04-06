90955a36da1e8c171cf6c8bc59d0fb95ad5f3acd
Deepak Suthar <suthardeepak658@gmail.com>
2026-04-06 21:49:31 +0530

    updated controller and added new endpoints

--- src/main/java/firstSpringProgram/FirstSpringProgram/HelloController.java
+++ src/main/java/firstSpringProgram/FirstSpringProgram/HelloController.java
@@ -1,18 +1,50 @@
 package firstSpringProgram.FirstSpringProgram;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "hello Deepak!";
-    }
-    
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to Spring Boot";
-    }
+	List<User> users = new ArrayList<>();
+//    @GetMapping("/hello")
+//    public String sayHello() {
+//        return "hello Deepak!";
+//    }
+//    
+//    @GetMapping("/")
+//    public String home() {
+//        return "Welcome to Spring Boot";
+//    }
+	
+	@PostMapping("/addUser")
+	public String addUser(@RequestBody User user)
+	{
+		users.add(user);
+		return "User added";
+	}
+	
+	@GetMapping("/getUsers")
+	public List<User> getUsers()
+	{
+		return users;
+	}
+	
+	@DeleteMapping("/deleteUser/{id}")
+	public String deleteUser(@PathVariable int id)
+	{
+		boolean removed = users.removeIf(u -> u.getId() == id);
+		
+		if(removed)
+			return "User deleted";
+		else
+			return "User not deleted";
+	}
 }
